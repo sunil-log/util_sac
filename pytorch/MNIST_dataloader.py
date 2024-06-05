e2338e7bbf62f8d555f77e32b2f60f6f1593d17f
Sunil Kim <celete01@gmail.com>
2024-06-05 12:32:46 +0900

    MNIST dataloader

--- pytorch/MNIST_dataloader.py
+++ pytorch/MNIST_dataloader.py
@@ -3,52 +3,42 @@
 import torchvision
 from torch.utils.data import DataLoader, Dataset
 
+"""
+MNIST class 는 dataloader 에 넣을 때 transform 을 한번에 적용하는 것이 아니라,
+dataloader 에 의해서 호출될 때마다 transform 을 적용한다. 그래서 느려진다.
+차라리 data 를 transform 한 뒤 torch tensor 로 변환하여 dataloader 에 넣는게 낫다.
+"""
+
 
 class MNIST_dataset(Dataset):
-
-	def __init__(self, data):
+	def __init__(self, data, targets):
 		self.data = data
+		self.targets = targets
 
 	def __len__(self):
 		return len(self.data)
 
 	def __getitem__(self, idx):
-		return self.data[idx]
+		return self.data[idx], self.targets[idx]
 
 
 def prepare_data(batch_size=64, train=True, flatten=True):
-	"""
-	MNIST class 는 dataloader 에 넣을 때 transform 을 한번에 적용하는 것이 아니라,
-	dataloader 에 의해서 호출될 때마다 transform 을 적용한다. 그래서 느려진다.
-	차라리 data 를 transform 한 뒤 torch tensor 로 변환하여 dataloader 에 넣는게 낫다.
-	"""
-
 	if train:
 		data = torchvision.datasets.MNIST(root='./data',
-											train=True,
-											download=True)
+		                                  train=True,
+		                                  download=True)
 	else:
 		data = torchvision.datasets.MNIST(root='./data',
-											train=False,
-											download=True)
+		                                  train=False,
+		                                  download=True)
 
+	images = data.data.float() / 255.0
+	targets = data.targets
 
-	# extract data from MNIST dataset
-	data = data.data        # torch.Size([60000, 28, 28])
-
-
-	# 0~255 인 data 를 0~1 사이의 값으로 normalize
-	data = data.float() / 255.0
-	print(f"Range of data: [{data.min()}, {data.max()}]")
-
-	# data 를 1차원으로 변환
 	if flatten:
-		data = data.view(-1, 28*28)
+		images = images.view(-1, 28 * 28)
 
-	# data 를 MNIST_dataset class 로 변환
-	dataset = MNIST_dataset(data)
-
-	# data 를 dataloader 로 변환
+	dataset = MNIST_dataset(images, targets)
 	dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
 	return dataloader
