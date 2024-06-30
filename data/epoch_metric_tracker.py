361a666b8bd35ddb1b16ef685ccb2128b628c07a
Sunil Kim <celete01@gmail.com>
2024-07-01 01:30:57 +0900

    metric_tracker 업데이트

--- data/epoch_metric_tracker.py
+++ data/epoch_metric_tracker.py
@@ -15,12 +15,12 @@ class Metric:
 	def get_all(self):
 		return self.data
 
+	def get_epochs_and_values(self):
+		return list(map(list, zip(*self.data)))
+
 	def get_values(self):
 		return [value for _, value in self.data]
 
-	def get_average(self):
-		values = self.get_values()
-		return np.mean(values) if values else None
 
 
 class metric_tracker:
@@ -36,39 +36,38 @@ class metric_tracker:
 	def get_latest(self, metric_name):
 		if metric_name in self.metrics:
 			latest = self.metrics[metric_name].get_latest()
-			return latest[1] if latest else None
+			return latest if latest else None
 		return None
 
 	def get_all(self, metric_name):
+		"""
+		Example:
+			x, y = tm["train_loss"]
+		"""
 		if metric_name in self.metrics:
-			return self.metrics[metric_name].get_all()
+			return self.metrics[metric_name].get_epochs_and_values()
 		return []
 
 	def __getitem__(self, metric_name):
+		"""
+		Example:
+			x, y = tm["train_loss"]
+		"""
 		return self.get_all(metric_name)
 
 	def get_metrics(self):
 		return list(self.metrics.keys())
 
-	def get_average(self, metric_name):
-		if metric_name in self.metrics:
-			return self.metrics[metric_name].get_average()
-		return None
-
 	def print_latest(self):
-		output = ", ".join(
-			f"{metric_name}: {self.get_latest(metric_name) if self.get_latest(metric_name) is not None else '데이터 없음'}"
-			for metric_name in self.get_metrics()
-		)
-		print(output, flush=True)
+		metric_names = self.get_metrics()
+		list_value = []
+		for metric_name in metric_names:
+			latest = self.get_latest(metric_name)
+			if latest is not None:
+				list_value.append(f"{metric_name}: {latest[1]:.4f}")
+			else:
+				list_value.append(f"{metric_name}: None")
+		curretn_epoch = latest[0]
+		print_str = f"Epoch {curretn_epoch} | " + " | ".join(list_value)
+		print(print_str, flush=True)
 
-	def remove_metric(self, metric_name):
-		if metric_name in self.metrics:
-			del self.metrics[metric_name]
-
-	def clear_data(self, metric_name=None):
-		if metric_name:
-			if metric_name in self.metrics:
-				self.metrics[metric_name] = Metric(metric_name)
-		else:
-			self.metrics.clear()
