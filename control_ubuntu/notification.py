1330ab525203c608e3495266e94ff4d802353aa4
Sunil Kim <celete01@gmail.com>
2024-11-01 00:42:58 +0900

    ubuntu related

---
+++ control_ubuntu/notification.py
@@ -0,0 +1,67 @@
+
+"""
+cron 에서 사용할 경우, DISPLAY 환경변수가 없어서 오류가 발생하는 경우가 있습니다.
+이럴 경우, 아래와 같이 DISPLAY 환경변수를 설정해주면 됩니다.
+	# specify display
+	# echo $DISPLAY
+	export DISPLAY=:1
+"""
+import subprocess
+import pandas as pd
+
+
+def convert_dict_to_str(my_dict):
+	"""
+	딕셔너리를 문자열로 변환하는 함수입니다.
+
+	Args:
+	my_dict (dict): 변환할 딕셔너리입니다.
+
+	Returns:
+	str: 딕셔너리를 문자열로 변환한 결과입니다.
+	"""
+	return pd.DataFrame(my_dict, index=[0]).T.to_string(header=False)
+
+
+
+def linux_alert(title, message):
+	"""
+	Linux에서 경고음을 내고 메시지 박스를 띄우는 함수입니다.
+
+	Args:
+	title (str): 메시지 박스의 제목입니다.
+	message (str): 메시지 박스에 표시할 메시지입니다.
+
+	Install beep and zenity:
+		sudo apt install beep zenity
+	"""
+	# 경고음 발생
+	# subprocess.Popen(['/usr/bin/paplay', sound_file_path])
+	# 메시지 박스 띄우기
+
+	# if type of message is dict, convert it to string
+	if isinstance(message, dict):
+		message = convert_dict_to_str(message)
+
+	subprocess.run(['/usr/bin/zenity', '--info', '--title', title, '--text', message])
+
+
+
+def linux_notification(title, message):
+	"""
+	Linux에서 알림을 띄우는 함수입니다.
+	다만 notify-send 는 cron 에서 사용하기 어렵습니다.
+
+	Args:
+	title (str): 알림의 제목입니다.
+	message (str): 알림에 표시할 메시지입니다.
+
+	Install notify-send:
+		sudo apt install libnotify-bin
+	"""
+
+	# if type of message is dict, convert it to string
+	if isinstance(message, dict):
+		message = convert_dict_to_str(message)
+
+	subprocess.run(['/usr/bin/notify-send', title, message])
---
+++ control_ubuntu/wmctrl.py
@@ -0,0 +1,30 @@
+
+import subprocess
+import pandas as pd
+
+def get_wmctrl_output_as_df():
+	"""
+	`wmctrl -l` 명령어의 출력을 파싱하여 pandas DataFrame으로 반환합니다.
+	각 행은 시스템에서 실행 중인 창 하나를 나타내며, 열은 창 ID, 데스크탑 번호, 호스트 이름, 창 제목을 포함합니다.
+	"""
+	# `wmctrl -l` 명령어 실행
+	process = subprocess.Popen(['wmctrl', '-l'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	stdout, stderr = process.communicate()
+
+	# 출력을 디코딩하고 각 라인으로 분리
+	output_lines = stdout.decode('utf-8').strip().split('\n')
+
+	# 출력 파싱
+	data = []
+	for line in output_lines:
+		parts = line.split(maxsplit=3)
+		if len(parts) == 4:
+			data.append(parts)
+		else:
+			# 예상치 못한 형식의 라인 처리
+			print(f"Unexpected line format: {line}")
+
+	# DataFrame 생성
+	df = pd.DataFrame(data, columns=['Window ID', 'Desktop No', 'Host Name', 'Window Title'])
+
+	return df
