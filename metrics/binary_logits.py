9e92308d459bcb3f1a1503853d87307b729191bf
Sunil Kim <celete01@gmail.com>
2025-02-14 22:06:53 +0900

    Add plot_confusion_matrix function to plot confusion matrices in plot_metrics.py

--- metrics/binary_logits.py
+++ metrics/binary_logits.py
@@ -101,36 +101,6 @@ def calculate_confusion_matrix(d):
 	return res
 
 
-def plot_confusion_matrix(cm, ax, f1=0.5):
-	"""
-	Draws a confusion matrix on the given Axes object.
-
-	Parameters:
-	cm (numpy.ndarray): Confusion matrix.
-	ax (matplotlib.axes.Axes): Axes object to draw the confusion matrix on.
-	"""
-	cax = ax.imshow(cm, cmap='Blues', interpolation='none')
-	ax.figure.colorbar(cax, ax=ax)
-	ax.set_title(f'Confusion Matrix, Binary F1: {f1:.3f}')
-
-	# 축 레이블 설정
-	tick_marks = np.arange(len(['True', 'False']))
-	ax.set_xticks(tick_marks)
-	ax.set_xticklabels(['Predicted False', 'Predicted True'])
-	ax.set_yticks(tick_marks)
-	ax.set_yticklabels(['True False', 'True True'])
-
-	# 텍스트 주석 추가
-	thresh = cm.mean()
-	for i, j in np.ndindex(cm.shape):
-		color = "white" if cm[i, j] > thresh else "black"
-		ax.text(j, i, format(cm[i, j], 'd'), ha="center", va="center", color=color)
-
-	ax.set_ylabel('True label')
-	ax.set_xlabel('Predicted label')
-	ax.figure.tight_layout()
-
-
 
 def binary_metrics(train_data, valid_data, test_data):
 
