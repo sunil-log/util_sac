9f2fa048b8796ac375d690cfd9d9c25f50d1c61e
sac <celete01@gmail.com>
2025-03-21 09:55:04 +0900

    Add function to load hyperparameters from JSON files into a DataFrame

--- pytorch/trainer/load_metrics.py
+++ pytorch/trainer/load_metrics.py
@@ -23,6 +23,44 @@ from util_sac.pandas.print_df import print_partial_markdown
 from util_sac.pandas.save_npz import load_df_from_npz
 from util_sac.dict.save_args import load_args
 
+import pandas as pd
+
+
+def load_hyperparams(base_path, keywords):
+	"""
+	주어진 keywords 리스트를 모두 포함하는 디렉토리를 찾아,
+	hyperparameters.json 파일만 읽어서 하나의 DataFrame으로 합친다.
+	각 DataFrame에 디렉토리 이름에서 추출한 trial_id를 추가한다.
+
+	Parameters:
+	- base_path (pathlib.Path): 탐색할 기준 경로
+	- keywords (list of str): 디렉토리 이름에 포함되어야 하는 문자열들의 리스트
+
+	Returns:
+	- pd.DataFrame 혹은 None: 모든 json 데이터를 합친 DataFrame.
+	  조건에 맞는 디렉토리가 없으면 None을 반환한다.
+	"""
+	dfs = []
+
+	for directory in base_path.iterdir():
+		if directory.is_dir() and all(kw in directory.name for kw in keywords):
+			json_path = directory / 'hyperparameters.json'
+			if json_path.exists():
+				# hyperparameters.json 로드
+				args = load_args(json_path)
+				# DataFrame 생성
+				temp_df = pd.DataFrame([args])
+				# trial_id 부여
+				temp_df['trial_time'] = directory.name.split('__')[0]
+				dfs.append(temp_df)
+
+	if not dfs:
+		print(f"No directory with keywords {keywords} found in {base_path}")
+		return None
+
+	df = pd.concat(dfs, axis=0, ignore_index=True)
+	return df
+
 
 def load_metrics(base_path, keywords):
 	"""
