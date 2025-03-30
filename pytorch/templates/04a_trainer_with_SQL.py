1eee9449d7258f20e1b8cdd2b7a1beca1c87624e
Sunil Kim <celete01@gmail.com>
2025-03-31 01:56:20 +0900

    Refactor save_args to save_json for consistency across trainer scripts

--- pytorch/templates/04a_trainer_with_SQL.py
+++ pytorch/templates/04a_trainer_with_SQL.py
@@ -9,7 +9,7 @@ Created on  Mar 01 2025
 
 
 import torch
-import matplotlib.pyplot as plt
+import numpy as np
 
 from util_sac.data.trial_manager2 import trial_manager
 from util_sac.data.epoch_metric_tracker import metric_tracker
@@ -17,9 +17,13 @@ from util_sac.pytorch.trainer.trainer import BaseTrainer
 from util_sac.pytorch.trainer.update_lr import current_lr
 from util_sac.pytorch.load_data.move_device import move_dict_tensors_to_device
 from util_sac.pytorch.metrics.multiclass_f1 import calculate_f1
-from util_sac.dict.json_manager import save_json
+from util_sac.dict.json_manager import save_json, load_json
 from util_sac.pandas.save_npz import save_df_as_npz
 from util_sac.sys.dir_manager import create_dir
+from util_sac.dict.jsonl_file_manager import jsonl_file_manager
+from util_sac.pytorch.optuna.get_objective import generate_lr_schedules
+
+
 
 import time
 import optuna
@@ -27,12 +31,16 @@ from types import SimpleNamespace
 from util_sac.pytorch.optuna.get_objective import get_objective
 
 
+import matplotlib.pyplot as plt
 
-lr_dicts = [
-    {10: 1e-4, 40: 1e-5},
-    {50: 1e-4, 80: 1e-5},
-    {99: 1e-4, 100: 1e-5}
-]
+
+n_epoch = 15
+n_trials = 100
+lr_dicts = generate_lr_schedules(
+	num_schedules=50,
+	total_epochs=n_epoch
+)
+
 
 
 param_space = {
@@ -109,7 +117,12 @@ def train_session(args):
 
 	# 2) trial manager
 	sub_dir_list = ["weights", "reconstruction", "latent_space"]
-	tm = trial_manager(sub_dir_list, trial_name=args.trial_name, zip_src_loc="../")
+	tm = trial_manager(
+		sub_dir_list,
+		base_dir=args.db_dir,
+		trial_name=args.trial_name,
+		zip_src_loc="../"
+	)
 
 	# 3) load data
 	dataloader_train = None
@@ -134,7 +147,7 @@ def train_session(args):
 		optimizer=optimizer,
 		criterion=criterion,
 		lr_dict=args.lr_dict,
-		n_epoch=100,
+		n_epoch=args.n_epoch,
 		args=args,
 	)
 
@@ -152,43 +165,79 @@ def train_session(args):
 		f1_valid = calculate_f1(valid_data, name="valid")
 		f1_test = calculate_f1(test_data, name="test")
 
-		mt.update(epoch, **train_loss, **test_loss, **f1_train, **f1_test, **lr)
+		mt.update(
+			epoch,
+			**train_loss, **valid_loss, **test_loss,
+			**f1_train, **f1_valid, **f1_test,
+			**lr
+		)
 		mt.print_latest()
 
 		if epoch % 10 == 0:
-
 			# save model
-			torch.save(model.state_dict(), f"{tm['weights']}/epoch_{epoch}.pth")
-
-			# plot the train and test metrics
-			plt.close()
-			fig, axes = plt.subplots(2, 3, figsize=(20, 10))
-
-			# plot losses
-			mt.plot_metric(axes[0, 0], keys=["train_loss", "valid_loss", "test_loss"], y_log='log')
-			mt.plot_metric(axes[0, 1], keys=["train_accuracy", "test_accuracy"])
-			mt.plot_metric(axes[0, 2], keys=["f1_class_macro_train", "f1_class_macro_test"])
-			mt.plot_metric(axes[1, 0], keys=["lr"], y_log='log')
-			plt.tight_layout()
-			plt.savefig(tm.trial_dir / "train_test_loss.png")
+			# torch.save(model.state_dict(), f"{tm['weights']}/epoch_{epoch}.pth")
+			pass
 
 
+	# plot the train and test metrics
+	plt.close()
+	fig, axes = plt.subplots(2, 3, figsize=(20, 10))
 
-		# save metrics
-		df_metrics = mt.generate_df()
-		df_metrics.to_csv(f"{tm.trial_dir}/train_test_metrics.csv", index=False)
-		save_df_as_npz(df_metrics, f"{tm.trial_dir}/train_test_metrics.npz")
-
-		# best score
-		best_f1 = df_metrics["f1_class_macro_test"].max()
-
-
-		# save hyperparameters
-		args.best_score = best_f1
-		save_json(args, f"{tm.trial_dir}/hyperparameters.json")
+	# plot losses
+	mt.plot_metric(axes[0, 0], keys=["train_loss", "valid_loss", "test_loss"], y_log='log')
+	mt.plot_metric(axes[0, 1], keys=["train_accuracy", "test_accuracy"])
+	mt.plot_metric(axes[0, 2], keys=["f1_class_macro_train", "f1_class_macro_test"])
+	mt.plot_metric(axes[1, 0], keys=["lr"], y_log='log')
+	plt.tight_layout()
+	plt.savefig(tm.trial_dir / "train_test_loss.png")
 
 
-		return best_f1
+	# save metrics
+	df_metrics = mt.generate_df()
+	df_metrics.to_csv(f"{tm.trial_dir}/train_test_metrics.csv", index=False)
+	save_df_as_npz(df_metrics, f"{tm.trial_dir}/train_test_metrics.npz")
+
+	# best score
+	best_f1 = df_metrics["f1_class_macro_test"].max()
+
+
+	# save hyperparameters
+	args.best_score = best_f1
+	save_json(args, f"{tm.trial_dir}/hyperparameters.json")
+
+
+	return best_f1
+
+
+
+def multiple_train_sessions(args):
+
+	# fn results
+	fn_results = f"{args.db_dir}/scores.jsonl"
+	jm = jsonl_file_manager(fn_results)
+
+	# trial_name
+	trial_name = args.trial_name
+	list_score = []
+	for i in range(3):
+
+		# modify args
+		args.trial_name = f"{trial_name}__session_{i}"
+		args.n_epoch = n_epoch
+		score = train_session(args)
+		list_score.append(score)
+
+
+	# save session results
+	d = vars(args)
+	d.pop("trial_name")
+	for i, score in enumerate(list_score):
+		d[f"score_{i}"] = score
+	d["mean_score"] = np.mean(list_score)
+	jm.write_line(d)
+
+	# average score
+	return d["mean_score"]
 
 
 
@@ -196,42 +245,35 @@ def train_session(args):
 def main():
 
 	"""
-	main
+	optuna_db 라는 이름의 DB가 존재한다고 가정
 	"""
 
+	d = load_json('/sac/mysql.json')
 	"""
-	1. Single Trial
-	"""
-	args_dict = {
-		"trial_name": "Building",
-		"input_dim": 32,
-		"n_head": 8,
-		"q_dim": 16,
+	{
+		'ip': '34.46.56.108', 
+		'port': '3306', 
+		'db_name': 'optuna_db', 
+		'id': 'root', 
+		'pass': 'xxx'
 	}
-	args = SimpleNamespace(**args_dict)
-	score = train_session(args)
-	print("Single Trial Score:", score)
-
-
 	"""
-	2. Optuna Optimization 
-	"""
+
 	# 1) study 생성 (이미 존재하면 로드)
-	study_name = f"ID_{time.strftime('%H%M%S')}__study_name"
+	study_name = f"{time.strftime('%H-%M-%S')}__study_name"
 	db_dir = f"./trials/{study_name}"
-	db_path = f"{db_dir}/study.db"
 	study_info = {
 		"study_name": study_name,
 		"db_dir": db_dir,
-		"db_path": db_path,
+		"db_path": f"mysql+pymysql://{d['id']}:{d['pass']}@{d['ip']}:{d['port']}/{d['db_name']}"
+
 	}
 	create_dir(db_dir)
-
 
 	# 2) study_name을 이용해 study 생성
 	study = optuna.create_study(
 		study_name=study_name,
-		storage=f"sqlite:///{db_path}",
+		storage=study_info["db_path"],   # 여기서 실제 URL 문자열을 사용
 		load_if_exists=True,
 		direction="maximize"
 	)
@@ -241,13 +283,12 @@ def main():
 		study_info=study_info,
 		param_space=param_space,
 		lr_dicts=lr_dicts,
-		train_sessions=train_session
+		train_sessions=multiple_train_sessions      # or train_session
 	)
-	study.optimize(objective_func, n_trials=100)
+	study.optimize(objective_func, n_trials=n_trials)
 
 	print("Best value:", study.best_value)
 	print("Best params:", study.best_params)
-
 
 
 if __name__ == "__main__":
---
+++ pytorch/templates/04b_study_analysis.py
@@ -0,0 +1,61 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on  Mar 27 2025
+
+@author: sac
+"""
+
+
+import pandas as pd
+import numpy as np
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+
+from util_sac.sys.date_format import add_timestamp_to_string
+from util_sac.pandas.print_df import print_partial_markdown
+from util_sac.data.print_array_info import print_array_info
+from util_sac.image_processing.reduce_palette import reduce_palette
+from util_sac.dict.jsonl_file_manager import jsonl_file_manager
+
+from util_sac.dict.json_manager import load_json
+
+
+import optuna
+
+
+def main():
+	d = load_json('/sac/mysql.json')
+	"""
+	{
+		'ip': '34.46.56.108', 
+		'port': '3306', 
+		'db_name': 'optuna_db', 
+		'id': 'root', 
+		'pass': 'xxx'
+	}
+	"""
+
+	# 이미 MySQL 등에 저장된 study를 load한다:
+	study = optuna.load_study(
+		study_name="16-11-30__study_name",
+		storage=f"mysql+pymysql://{d['id']}:{d['pass']}@{d['ip']}:{d['port']}/{d['db_name']}"
+	)
+
+	# Trial 정보들을 DataFrame으로 불러온다
+	df = study.trials_dataframe()
+	print_partial_markdown(df)
+	"""
+	|    |   number |      value | datetime_start      | datetime_complete   | duration        |   params_input_dim |   params_lr_dict_idx |   params_n_head |   params_q_dim | state    |
+	|---:|---------:|-----------:|:--------------------|:--------------------|:----------------|-------------------:|---------------------:|----------------:|---------------:|:---------|
+	|  0 |        0 | 0.133748   | 2025-03-26 16:07:37 | 2025-03-26 16:07:48 | 0 days 00:00:11 |                  4 |                   27 |              32 |              4 | COMPLETE |
+	|  1 |        1 | 0.370564   | 2025-03-26 16:07:57 | 2025-03-26 16:08:09 | 0 days 00:00:12 |                128 |                    0 |             128 |              8 | COMPLETE |
+	|  2 |        2 | 0.287456   | 2025-03-26 16:08:18 | 2025-03-26 16:08:29 | 0 days 00:00:11 |                128 |                   25 |               2 |              8 | COMPLETE |
+	|  3 |        3 | 0.854703   | 2025-03-26 16:08:38 | 2025-03-26 16:08:49 | 0 days 00:00:11 |                  2 |                   18 |               2 |              2 | COMPLETE |
+	|  4 |        4 | 0.790882   | 2025-03-26 16:08:58 | 2025-03-26 16:09:10 | 0 days 00:00:12 |                128 |                   11 |              16 |              4 | COMPLETE |
+	"""
+
+
+if __name__ == "__main__":
+	main()
