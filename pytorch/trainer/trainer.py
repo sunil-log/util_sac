e15f98dfc7f19855f1ab3694b7623a4432a5b410
Sunil Kim <celete01@gmail.com>
2025-04-02 11:28:17 +0900

    Refactor import statements in trainer.py for improved clarity

--- pytorch/trainer/trainer.py
+++ pytorch/trainer/trainer.py
@@ -1,8 +1,9 @@
 import torch
 from pathlib import Path
 import pandas as pd
-from util_sac.pytorch.data import batch_loss_collector, batch_data_collector
+from util_sac.pytorch.data.batch_data_collector import batch_loss_collector, batch_data_collector
 from util_sac.pytorch.trainer.update_lr import update_lr_with_dict
+
 
 """
 trainer.py 대비 dataloader 를 dict 로 모아서 받는다.
