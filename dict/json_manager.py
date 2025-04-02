615b0d87f9a687ceb920c12e78213878438c17d4
Sunil Kim <celete01@gmail.com>
2025-04-02 14:44:24 +0900

    Enhance save_json function to support types.SimpleNamespace in addition to argparse.Namespace

--- dict/json_manager.py
+++ dict/json_manager.py
@@ -7,20 +7,21 @@ Created on  Mar 15 2025
 """
 
 
-import argparse
 import json
+import argparse
+from types import SimpleNamespace
 
 def save_json(args, filename):
 	"""
-	'args'가 'argparse.Namespace' 형식이면 vars() 함수를 사용하여 dict로 변환하고,
+	'args'가 'argparse.Namespace' 혹은 'types.SimpleNamespace' 형식이면 vars() 함수를 사용하여 dict로 변환하고,
 	이미 dict라면 그대로 JSON 파일로 저장한다.
 	"""
-	if isinstance(args, argparse.Namespace):
+	if isinstance(args, argparse.Namespace) or isinstance(args, SimpleNamespace):
 		data = vars(args)
 	elif isinstance(args, dict):
 		data = args
 	else:
-		raise ValueError("지원되지 않는 args 타입이다. 'argparse.Namespace' 또는 'dict'만 가능하다.")
+		raise ValueError("지원되지 않는 args 타입이다. 'argparse.Namespace', 'types.SimpleNamespace' 또는 'dict'만 가능하다.")
 
 	with open(filename, "w", encoding="utf-8") as f:
 		json.dump(data, f, indent=4, ensure_ascii=False)
