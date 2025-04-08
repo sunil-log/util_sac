58c58e3e409eea10bf26642e121b00a48c34ea5e
Sunil Kim <celete01@gmail.com>
2025-04-09 02:09:36 +0900

    Add run_session function to manage training and optimization with Optuna

--- pytorch/optuna/sessions.py
+++ pytorch/optuna/sessions.py
@@ -20,8 +20,9 @@ from util_sac.image_processing.reduce_palette import reduce_palette
 
 
 from util_sac.dict.jsonl_file_manager import jsonl_file_manager
-
+from util_sac.pytorch.optuna.get_objective import get_objective
 from copy import deepcopy
+from util_sac.pytorch.optuna.get_objective import generate_lr_schedules
 
 
 
@@ -78,6 +79,68 @@ def train_multiple_sessions(train_session, args):
 	jm.write_line(d)
 
 	return d["mean_score"]
+
+
+def run_session(config, train_session):
+
+	# lr_dicts 생성
+	lr_dicts = generate_lr_schedules(
+		num_schedules=50,
+		total_epochs=config["static"]["n_epoch"]
+	)
+
+
+	if 'optimize' not in config or config['optimize'] is None:
+
+		"""
+		Only Static section
+		"""
+
+		args = config["static"]
+		args["lr_dict"] = lr_dicts[args["lr_dict_idx"]]
+		score = train_multiple_sessions(train_session, args)
+
+		print("Single Trial Score:", score)
+
+
+	else:
+		"""
+		Optuna optimize section
+		"""
+
+		# get params
+		args_static = config["static"]
+		param_space = config["optimize"]
+
+		# add params
+		args_static["db_path"] = f"{args_static['db_dir']}/study.db"
+		study_info = {
+			"study_name": args_static["study_id"],
+			"db_dir": args_static["db_dir"],
+			"db_path": args_static["db_path"],
+		}
+
+		# 2) study_name을 이용해 study 생성
+		study = optuna.create_study(
+			study_name=study_info["study_name"],
+			storage=f"sqlite:///{study_info['db_path']}",
+			load_if_exists=True,
+			direction="maximize"
+		)
+
+		# get_objective(study_name)로부터 objective 함수를 얻어서 optimize
+		objective_func = get_objective(
+			args_static=args_static,
+			param_space=param_space,
+			lr_dicts=lr_dicts,
+			train_session=train_session
+		)
+		study.optimize(objective_func, n_trials=args_static["n_trials"])
+
+		print("Best value:", study.best_value)
+		print("Best params:", study.best_params)
+
+
 
 
 
