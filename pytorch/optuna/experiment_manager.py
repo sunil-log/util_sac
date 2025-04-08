fa1e3400a64b22eafaff20300968a32f8b609755
Sunil Kim <celete01@gmail.com>
2025-04-09 02:22:30 +0900

    Refactor experiment_manager.py to integrate Optuna for session training and optimization

--- pytorch/optuna/experiment_manager.py
+++ pytorch/optuna/experiment_manager.py
@@ -7,16 +7,13 @@ Created on  Apr 09 2025
 """
 
 
-import pandas as pd
-import numpy as np
 from pathlib import Path
+import optuna
 
-import matplotlib.pyplot as plt
 
-from util_sac.sys.date_format import add_timestamp_to_string
-from util_sac.pandas.print_df import print_partial_markdown
-from util_sac.pytorch.data.print_array import print_array_info
-from util_sac.image_processing.reduce_palette import reduce_palette
+from util_sac.pytorch.optuna.session_manager import train_multiple_sessions
+from util_sac.pytorch.optuna.get_objective import get_objective, generate_lr_schedules
+
 
 
 
@@ -74,7 +71,7 @@ def execute_experiment(config, train_session):
 			lr_dicts=lr_dicts,
 			train_session=train_session
 		)
-		study.optimize(objective_func, n_trials=args_static["n_trials"])
+		study.optimize(objective_func, n_trials=args_static["n_trial"])
 
 		print("Best value:", study.best_value)
 		print("Best params:", study.best_params)
